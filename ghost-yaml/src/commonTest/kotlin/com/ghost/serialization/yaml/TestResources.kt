c8689fd9b0a1bb6322d91988bb6089e80192b806
Juan Hurtado <juan1402.91@gmail.com>
2026-06-24 20:35:19 -0600

    test(yaml): update unit tests imports and references to match restructured packages

--- ghost-yaml/src/commonTest/kotlin/com/ghost/serialization/yaml/TestResources.kt
+++ ghost-yaml/src/commonTest/kotlin/com/ghost/serialization/yaml/TestResources.kt
@@ -1,4 +1,7 @@
 package com.ghost.serialization.yaml
+import com.ghost.serialization.yaml.parser.GhostYamlFlatReader
+import com.ghost.serialization.yaml.writer.GhostYamlFlatWriter
+import com.ghost.serialization.yaml.writer.GhostYamlWriter
 
 /**
  * Reads a test resource file by path (relative to commonTest/resources).
