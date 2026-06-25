625e898c8be07fd091a032711acc5f43b375eefa
Juan Hurtado <juan1402.91@gmail.com>
2026-06-24 20:37:52 -0600

    refactor(yaml): remove conflicting default serialize and deserialize signatures from GhostYamlSerializer interface

--- ghost-yaml/src/commonMain/kotlin/com/ghost/serialization/yaml/contract/GhostYamlSerializer.kt
+++ ghost-yaml/src/commonMain/kotlin/com/ghost/serialization/yaml/contract/GhostYamlSerializer.kt
@@ -7,16 +7,7 @@ import okio.BufferedSink
 import okio.BufferedSource
 
 interface GhostYamlSerializer<T> {
-    fun serialize(sink: BufferedSink, value: T) {
-        val writer = GhostYamlWriter(sink)
-        serialize(writer, value)
-        writer.flush()
-    }
     fun serialize(writer: GhostYamlWriter, value: T)
     fun serialize(writer: GhostYamlFlatWriter, value: T)
-    fun deserialize(source: BufferedSource): T {
-        val reader = GhostYamlFlatReader(source.readByteArray())
-        return deserialize(reader)
-    }
     fun deserialize(reader: GhostYamlFlatReader): T
 }
